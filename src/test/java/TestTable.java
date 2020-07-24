911a1dbd847f10069462c040e2c58712812118f0
Magdalena Dziedzic <magda.dziedzic1991@gmail.com>
2020-07-24 14:36:17 +0200

    add java class

--- src/test/java/TestTable.java
+++ src/test/java/TestTable.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
-
 public class TestTable {
+    public static void main(String[] args) {
+
+        int[] numbers = new int[10];
+        numbers[0] = 10;
+        numbers[1] = 9;
+        numbers[2] = 8;
+        numbers[3] = 7;
+        numbers[4] = 6;
+        numbers[5] = 5;
+        numbers[6] = 4;
+        numbers[7] = 3;
+        numbers[8] = 2;
+        numbers[9] = 1;
+
+        System.out.println("pobrany argument to: " + numbers[1]);
+
+        if ((numbers[1] % 2)==1) {
+            System.out.println("liczba " + numbers[1] + " jest nieparzysta");
+        } else
+            System.out.println("jest parzysta");
+
+
+    }
 }
