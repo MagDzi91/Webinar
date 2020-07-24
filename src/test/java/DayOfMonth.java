4bcae91b4d5b195f2d67d5a6da0289abdcbdd7c1
Magdalena Dziedzic <magda.dziedzic1991@gmail.com>
2020-07-24 15:41:48 +0200

    add class

--- src/test/java/DayOfMonth.java
+++ src/test/java/DayOfMonth.java
@@ -6,28 +6,32 @@ public class DayOfMonth {
     public static void main(String[] args) {
 
         String[] month = new String[13];
-        month[0]="Zacznij od jedynki :P";
-        month[1]= "Styczeń";
-        month[2]= "Luty";
-        month[3]= "Marzec";
-        month[4]= "Kwiecień";
-        month[5]= "Maj";
-        month[6]= "Czerwiec";
-        month[7]="Lipiec";
-        month[8]="Sierpień";
-        month[9]="Wrzesień";
-        month[10]="Październik";
-        month[11]="Listopad";
-        month[12]="Grudzień";
+        month[0] = "Zacznij od jedynki :P";
+        month[1] = "Styczeń";
+        month[2] = "Luty";
+        month[3] = "Marzec";
+        month[4] = "Kwiecień";
+        month[5] = "Maj";
+        month[6] = "Czerwiec";
+        month[7] = "Lipiec";
+        month[8] = "Sierpień";
+        month[9] = "Wrzesień";
+        month[10] = "Październik";
+        month[11] = "Listopad";
+        month[12] = "Grudzień";
 
         System.out.println("Podaj numer miesiąca");
         Scanner scan = new Scanner(System.in);
         String name = scan.nextLine();
 
+        if (Integer.parseInt(name) > 12) {
+            System.out.println("nie ma takiego miesiąca");
+        } else {
 
-        Integer.parseInt(name);
-        System.out.println("Miesiąc to " + month[Integer.parseInt(name)] );
-
+            Integer.parseInt(name);
+            System.out.println("Miesiąc to " + month[Integer.parseInt(name)]);
+            
+        }
     }
 }
 
