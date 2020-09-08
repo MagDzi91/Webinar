91a0196eeabe16f8a74e03dad058dd5efa6dd4f3
Magdalena Dziedzic <magda.dziedzic1991@gmail.com>
2020-09-08 13:19:11 +0200

    poprawki

--- src/main/java/config/DriverHelper.java
+++ src/main/java/config/DriverHelper.java
@@ -3,6 +3,8 @@ package config;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.io.File;
+
 public class DriverHelper {
 
     static WebDriver driver;
@@ -15,7 +17,12 @@ public class DriverHelper {
     }
 
     private static WebDriver startDriver() {
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\MagdaDziedzic\\Desktop\\Chromedriver\\chromedriver.exe");
+        ClassLoader classLoader = DriverHelper.class.getClassLoader();
+        String chromeDriverPath = classLoader.getResource("chromedriver.exe").getFile();
+
+        File chromeDriver = new File(chromeDriverPath);
+
+        System.setProperty("webdriver.chrome.driver", chromeDriver.getAbsolutePath());
         driver = new ChromeDriver();
         driver.get("http://automationpractice.com/index.php");
         driver.manage().window().maximize();
