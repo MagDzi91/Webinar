a9ff05e4206c491c8e58869696cd4ec4cc3024d9
Magdalena Dziedzic <magda.dziedzic1991@gmail.com>
2020-09-08 13:06:30 +0200

    test

--- src/test/java/Webinar5/MultiplicationTable.java
+++ src/test/java/Webinar5/MultiplicationTable.java
@@ -1,25 +1,25 @@
-package Webinar5;
-
-import java.util.Scanner;
-
-public class MultiplicationTable {
-
-    public static void main(String[] args) {
-
-        System.out.println("podaj wartość x");
-        Scanner scan = new Scanner(System.in);
-        int x = scan.nextInt();
-
-        System.out.println("podaj wartość y");
-        int y = scan.nextInt();
-
-        int numbers[][] = new int[101][101];
-
-        for (int i = 0; i < 100; i++) {
-            for (int j = 0; j < 100; j++) {
-                numbers[j][i] = j * i;
-            }
-        }
-        System.out.println(numbers[x][y]);
-    }
-}
+//package Webinar5;
+//
+//import java.util.Scanner;
+//
+//public class MultiplicationTable {
+//
+//    public static void main(String[] args) {
+//
+//        System.out.println("podaj wartość x");
+//        Scanner scan = new Scanner(System.in);
+//        int x = scan.nextInt();
+//
+//        System.out.println("podaj wartość y");
+//        int y = scan.nextInt();
+//
+//        int numbers[][] = new int[101][101];
+//
+//        for (int i = 0; i < 100; i++) {
+//            for (int j = 0; j < 100; j++) {
+//                numbers[j][i] = j * i;
+//            }
+//        }
+//        System.out.println(numbers[x][y]);
+//    }
+//}
---
+++ src/test/java/Webinar5/costam.java
@@ -0,0 +1,4 @@
+package Webinar5;
+
+public class costam {
+}
